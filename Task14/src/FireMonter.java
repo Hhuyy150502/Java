798b8b5b5924ff559dd505985824f59980c06492
Hhuyy150502 <96615568+Hhuyy150502@users.noreply.github.com>
2022-05-11 03:53:57 +0700

    14

---
+++ Task14/src/FireMonter.java
@@ -0,0 +1,10 @@
+public class FireMonter extends Monster {
+    public FireMonter(String name) {
+        super(name);
+    }
+    @Override
+    public String attack() {
+        return "Attack with fire!";
+    }
+
+}
