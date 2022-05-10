ed466a58c5f3b573e378fd14b6cf9496d517a492
Hhuyy150502 <96615568+Hhuyy150502@users.noreply.github.com>
2022-05-11 03:54:09 +0700

    15

---
+++ Task15/src/Shape.java
@@ -0,0 +1,13 @@
+abstract class Shape {
+    private String color;
+    public Shape(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "color='" + color + '\'' +
+                '}';
+    }
+}
---
+++ Task15/src/TestShape.java
@@ -0,0 +1,7 @@
+import java.awt.*;
+
+public class TestShape {
+    public static void main(String[] args) {
+        Shape s1 = new Rectangle("red", 4, 5);
+    }
+}
