78d7510e4de139f066ba646716527a760312e0e1
Hhuyy150502 <96615568+Hhuyy150502@users.noreply.github.com>
2022-05-11 15:57:36 +0700

    Kiểm tra

---
+++ Bài kiểm tra/src/Account.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Account {
+    private String customerCode;
+    private String customerName;
+    private int accNumber;
+    private long amount;
+
+    public void input() {
+        do {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Customer identifier: ");
+            customerCode = sc.nextLine();
+            System.out.print("\n Customer name: ");
+            customerName = sc.nextLine();
+            System.out.print("\n Account identifier: ");
+            accNumber = sc.nextInt();
+            System.out.print("\n Account balance: ");
+            amount = sc.nextLong();
+        }while (accNumber<100000 || accNumber > 101000|| customerCode.length() < 5 );
+    }
+    public void depositAndWithdraw(long money, int type) {
+        System.out.println("Method to make deposit or withdrawal from bank account");
+        System.out.println("0. then this is bank deposit to add the amount of money to bank account");
+        System.out.println("1. hen this is bank withdrawal to subtract the amount of money from the  account balance");
+        switch (type) {
+            case 0:
+                if(money>0) {
+                    amount+=money;
+                    System.out.println("Successfully adding money: "+amount);
+                }else {
+                    System.out.println("ERR");
+                }
+                break;
+            case 1:
+                if(money>0 && money<amount) {
+                    amount-=money;
+                    System.out.println("Withdrawal successful: "+amount);
+                }else {
+                    System.out.println("ERR");
+                }
+                break;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "customerCode='" + customerCode + '\'' +
+                ", customerName='" + customerName + '\'' +
+                ", accNumber=" + accNumber +
+                ", amount=" + amount +
+                '}';
+    }
+
+}
--- Bài kiểm tra/src/TestAccount.java
+++ Bài kiểm tra/src/TestAccount.java
@@ -1,10 +1,8 @@
-package Examination;
-
 public class TestAccount {
     public static void main(String[] args){
         Account acc1 = new Account();
         acc1.input();
-        acc1.depositAndWithdraw();
+        acc1.depositAndWithdraw(100,0);
         System.out.println(acc1);
     }
 }
